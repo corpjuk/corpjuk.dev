759391b3bde901720e66931ac5b0eea384e48ecd
Justin Cunningham <juk@juk.dev>
2021-09-09 03:15:37 +0000

    fixed templates dirs

--- eatplants/settings/dev.py
+++ eatplants/settings/dev.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_BASE = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / "templates",
+            TEMPLATE_BASE / "templates",
         ],
         'APP_DIRS': True,
         'OPTIONS': {
