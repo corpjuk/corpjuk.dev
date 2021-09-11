4dcd866e997b74db11c1c84e22cd7d5cdedb7b56
Chris Shenkan <cshenkan7@gmail.com>
2021-09-11 03:12:50 -0400

    fixed prod setup

--- eatplants/settings/prod.py
+++ eatplants/settings/prod.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from os.path import join, normpath, dirname, abspath
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+LOG_ROOT = normpath(join(BASE_DIR, 'logs'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
