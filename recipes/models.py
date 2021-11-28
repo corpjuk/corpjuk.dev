d5565a57b3a96d1a8630d3bd3617579393cf87bf
corpjuk <kekejak@gmail.com>
2021-11-28 01:50:44 -0500

    added RecipeQuerySet and RecipeManager

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 import pint
 from django.conf import settings
+from django.db.models import Q
 from django.db import models
 from django.urls import reverse
 from .utils import number_str_to_float
@@ -15,6 +16,23 @@ from .validators import validate_unit_of_measure
         - Directions for Ingredients
 """
 
+class RecipeQuerySet(models.QuerySet):
+    def search(self, query=None):
+        if query is None or query == "":
+            return self.none() #this is the same as Article.objects.none(), but it's still an empty list []
+        lookups = (
+            Q(name__icontains=query) | 
+            Q(description__icontains=query) |
+            Q(directions__icontains=query)
+        )
+        return self.filter(lookups)
+
+class RecipeManager(models.Manager):
+    def get_queryset(self):
+        return RecipeQuerySet(self.model, using=self._db)
+    def search(self, query=None):
+        return self.get_queryset().search(query=query)
+
 class Recipe(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
@@ -24,6 +42,12 @@ class Recipe(models.Model):
     updated = models.DateTimeField(auto_now=True) 
     active = models.BooleanField(default=True)
 
+    objects = RecipeManager()
+
+    @property
+    def title(self):
+        return self.name
+
     def get_absolute_url(self):
         return reverse("recipes:detail", kwargs={"id": self.id})
 
@@ -32,6 +56,9 @@ class Recipe(models.Model):
 
     def get_edit_url(self):
         return reverse("recipes:update", kwargs={"id": self.id})
+    
+    def get_delete_url(self):
+        return reverse("recipes:delete", kwargs={"id": self.id})
 
     def get_ingredients_children(self):
         return self.recipeingredient_set.all()
@@ -52,12 +79,20 @@ class RecipeIngredient(models.Model):
     def get_absolute_url(self):
         return self.recipe.get_absolute_url()
 
+    def get_delete_url(self):
+        kwargs = {
+            "parent_id": self.recipe.id,
+            "id": self.id
+        }
+        return reverse("recipes:ingredient-delete", kwargs=kwargs)
+
     def get_hx_edit_url(self):
         kwargs = {
             "parent_id": self.recipe.id,
             "id": self.id
         }
         return reverse("recipes:hx-ingredient-detail", kwargs=kwargs)
+
 
     def convert_to_system(self, system="mks"):
         if self.quantity_as_float is None:
