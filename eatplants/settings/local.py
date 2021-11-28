bad9405cf330837ae2c04ed30738a5049cd21cb3
corpjuk <kekejak@gmail.com>
2021-11-28 01:50:04 -0500

    added search to apps

--- eatplants/settings/local.py
+++ eatplants/settings/local.py
@@ -51,8 +51,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_htmx',
-    'articles.apps.ArticlesConfig',
+    'articles',
     'recipes',
+    'search'
 ]
 
 MIDDLEWARE = [
